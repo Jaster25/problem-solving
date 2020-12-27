8d5e76e73be162282d98c377fe8b06f115610e19
Jaster25 <js_328@naver.com>
2020-12-27 11:50:41 +0900

    2020-12-27

---
+++ [2667] 단지번호붙이기.py
@@ -0,0 +1,43 @@
+from collections import deque
+from sys import stdin
+
+
+def solution():
+    N = int(stdin.readline())
+    MAP = [list(map(int, stdin.readline().strip())) for _ in range(N)]
+    # N = int(input())
+    # MAP = [list(map(int, input())) for _ in range(N)]
+    visited = [[False] * N for _ in range(N)]
+    cnt = 0
+    apart = []
+
+    def DFS(y, x):
+        visited[y][x] = True
+        nonlocal cnt
+        cnt += 1
+
+        dy = [-1, 1, 0, 0]
+        dx = [0, 0, -1, 1]
+
+        for i in range(4):
+            ny = y + dy[i]
+            nx = x + dx[i]
+            if 0 <= ny < N and 0 <= nx < N:
+                if MAP[ny][nx] and not visited[ny][nx]:
+                    DFS(ny, nx)
+
+    for i in range(N):
+        for j in range(N):
+            if MAP[i][j] and not visited[i][j]:
+                cnt = 0
+                DFS(i, j)
+                apart.append(cnt)
+
+    apart.sort()
+
+    print(len(apart))
+    for num in apart:
+        print(num)
+
+
+solution()
---
+++ [7576] 토마토.py
@@ -0,0 +1,45 @@
+from collections import deque
+from sys import stdin
+
+
+def solution():
+
+    N, M = map(int, stdin.readline().split())
+    tomato, deq = [], deque()
+    visited = [[0 for _ in range(N)] for _ in range(M)]
+
+    for i in range(M):
+        row = list(map(int, stdin.readline().split()))
+        tomato.append(row)
+        for j in range(N):
+            if row[j] == 1:
+                visited[i][j] = 1
+                deq.append([i, j])
+
+    # * BFS
+    dy = [1, -1, 0, 0]
+    dx = [0, 0, 1, -1]
+    while deq:
+        y, x = deq.popleft()
+        for i in range(4):
+            ny = y + dy[i]
+            nx = x + dx[i]
+            if 0 <= ny < M and 0 <= nx < N:
+                if tomato[ny][nx] == 0:
+                    tomato[ny][nx] = 1
+                    visited[ny][nx] = visited[y][x] + 1
+                    deq.append([ny, nx])
+
+    # * check
+    isAllRipe = True
+    for row in tomato:
+        if 0 in row:
+            isAllRipe = False
+
+    if isAllRipe:
+        print(max(map(max, visited)) - 1)
+    else:
+        print(-1)
+
+
+solution()

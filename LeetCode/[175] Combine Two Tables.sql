a5bb97650007fcf75454825984a4688b28ef4475
jaster25 <js_328@naver.com>
2022-05-18 10:26:51 +0900

    2022-05-18

--- LeetCode/[175] Combine Two Tables.sql
+++ LeetCode/[175] Combine Two Tables.sql
@@ -1,11 +1,4 @@
--- Runtime: 416 ms, faster than 31.39%
-SELECT P.FirstName, P.LastName, A.City, A.State
-FROM Person P LEFT JOIN Address A
-ON P.PersonId = A.PersonId;
-
--- Runtime: 315 ms, faster than 86.84%
-SELECT P.FirstName, P.LastName, A.City, A.State
-FROM Person P LEFT OUTER JOIN Address A
-ON P.PersonId = A.PersonId;
-
--- 같은 의미의 코드
+SELECT p.firstName, p.lastName, a.city, a.state
+FROM Person p
+LEFT JOIN Address a
+ON p.personId = a.personId;
